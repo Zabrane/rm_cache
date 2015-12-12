3b0531102388794272eb6a572e2c0840a7b366d3
Simon Zelazny <zelazny@mailbox.org>
2015-12-12 18:23:00 +0100

    Evict cold keys first; clear table if completely hot

--- src/rm_cache.erl
+++ src/rm_cache.erl
@@ -1,21 +1,25 @@
 -module(rm_cache).
--export([new/3, lookup/2, info/1]).
--define(MFA_KEY, 1).
--define(SIZE_KEY, 2).
+-export([new/3, lookup/2]).
+-export([info/1, to_list/1]).
+-define(META_MFA_KEY, 1).
+-define(META_SIZE_KEY, 2).
 
 new(Name, Size, {M,F,A}) ->
     ok = new_data_table(Name),
     ok = new_meta_table(Name, Size, {M,F,A}).
 
 lookup(Name, Key) ->
-    case ets:lookup(Name, Key) of
-        [{Key,V}] -> {ok, V};
-        [] -> ask_backend(Name, Key)
+    case find_in_cache(Name, Key) of
+        {ok, V} -> {ok, V};
+        {error, not_found_in_cache} -> ask_backend(Name, Key)
     end.
 
 info(Name) ->
     [MFA, Size] = [element(2,I) || I <- lists:sort(ets:tab2list(meta_table(Name)))],
     {ets:info(Name, name), Size, MFA}.
+
+to_list(Name) ->
+    ets:tab2list(Name).
 
 meta_table(Name) ->
     list_to_atom(atom_to_list(Name) ++ "_meta").
@@ -27,18 +31,39 @@ new_data_table(Name) ->
 new_meta_table(Name, Size, {_,_,_} = MFA) ->
     MetaName = meta_table(Name),
     ets:new(MetaName, [named_table, public]),
-    ets:insert(MetaName, {?MFA_KEY, MFA}),
-    ets:insert(MetaName, {?SIZE_KEY, Size}),
+    ets:insert(MetaName, {?META_MFA_KEY, MFA}),
+    ets:insert(MetaName, {?META_SIZE_KEY, Size}),
     ok.
 
 ask_backend(Name, Key) ->
-    {M,F,A} = get_mfa(Name),
+    {M,F,A} = meta_mfa(Name),
     case erlang:apply(M,F,A++[Key]) of
-        {ok, V} -> ets:insert(Name, {Key, V}),
+        {ok, V} -> insert_new_key(Name, Key,V),
                    {ok, V};
         Error -> Error
     end.
 
-get_mfa(Name) ->
-    [{1,MFA}] = ets:lookup(meta_table(Name),1),
-    MFA.
+find_in_cache(Name, Key) ->
+    case ets:lookup(Name, Key) of
+        [{Key, Val, hot}] -> {ok, Val};
+        [{Key, Val, cold}] -> ets:insert(Name, {Key, Val, hot}),
+                              {ok, Val};
+        _ -> {error, not_found_in_cache} end.
+
+insert_new_key(Name, Key, Value) ->
+    case ets:info(Name, size) >= meta_size(Name) of
+        false -> ets:insert(Name, {Key, Value, cold});
+        true -> evict_cold(Name, 1),
+                ets:insert(Name, {Key, Value, cold})
+    end.
+
+evict_cold(Name, Count) when Count > 0 ->
+    case ets:match(Name, {'$1','_',cold}, Count) of
+        {[[K]|_], _} -> ets:delete(Name, K);
+        '$end_of_table' -> ets:delete_all_objects(Name) end.
+
+meta_mfa(Name) -> meta(Name, ?META_MFA_KEY).
+meta_size(Name) ->meta(Name, ?META_SIZE_KEY).
+meta(Name, MetaKey) ->
+    [{MetaKey,V}] = ets:lookup(meta_table(Name),MetaKey),
+    V.
--- test/rm_cache_SUITE.erl
+++ test/rm_cache_SUITE.erl
@@ -5,7 +5,9 @@
 all() ->
     [ it_is_created_with_a_name_size_and_backend,
       it_is_created_with_a_backend_fun,
-      subsequent_lookup_does_not_hit_backend
+      subsequent_lookup_does_not_hit_backend,
+      cold_keys_are_evicted_first,
+      all_keys_are_evicted_if_table_fully_hot
     ].
 
 
@@ -18,21 +20,52 @@ it_is_created_with_a_name_size_and_backend(_) ->
 it_is_created_with_a_backend_fun(_) ->
   bddr:test
     (?Given() ->
-            {ok, B} = rm_test_backend:start([{<<"1">>,<<"a">>}]),
+            {ok, B} = rm_test_backend:start([{"1","a"}]),
             rm_cache:new(t2, 01, {rm_test_backend, retrieve, [B]}) end,
      ?When(_) ->
-            rm_cache:lookup(t2, <<"1">>) end,
+            rm_cache:lookup(t2, "1") end,
      ?Then(Res) ->
-            {ok, <<"a">>} = Res end).
-
+            {ok, "a"} = Res end).
 
 subsequent_lookup_does_not_hit_backend(_) ->
   bddr:test
     (?Given() ->
-            rm_test_backend:start(b3, [{<<"10">>,<<"aa">>}]),
+            rm_test_backend:start(b3, [{"10","aa"}]),
             rm_cache:new(t3, 10, {rm_test_backend, retrieve, [b3]}),
-            {ok, <<"aa">>} = rm_cache:lookup(t3, <<"10">>) end,
+            {ok, "aa"} = rm_cache:lookup(t3, "10") end,
      ?When(_) ->
-            {ok, <<"aa">>} = rm_cache:lookup(t3, <<"10">>) end,
+            rm_cache:lookup(t3, "10") end,
+     ?Then(Res) ->
+            {ok, "aa"} = Res,
+            {_, [_Call1]} = rm_test_backend:dump_state(b3) end).
+
+cold_keys_are_evicted_first(_) ->
+  bddr:test
+    (?Given() ->
+            rm_test_backend:start(rgb, [{"b","blue"}, {"g","green"}, {"r","red"}]),
+            rm_cache:new(t4, 2, {rm_test_backend, retrieve, [rgb]}),
+            {ok,"blue"} = rm_cache:lookup(t4, "b"),
+            hot_key(t4, "r") end,
+     ?When(_) ->
+            {ok, "green"} = rm_cache:lookup(t4, "g") end,
      ?Then(_) ->
-            {_Store, [_Call1]} = rm_test_backend:dump_state(b3) end).
+            [{"g", "green", cold}, {"r", "red", hot}] =
+                lists:sort(rm_cache:to_list(t4)) end).
+
+
+all_keys_are_evicted_if_table_fully_hot(_) ->
+  bddr:test
+    (?Given() ->
+            rm_test_backend:start(rgb, [{"b","blue"}, {"g","green"}, {"r","red"}]),
+            rm_cache:new(t5, 2, {rm_test_backend, retrieve, [rgb]}),
+            hot_key(t5, "r"),
+            hot_key(t5, "g") end,
+     ?When(_) ->
+            {ok, "blue"} = rm_cache:lookup(t5, "b") end,
+     ?Then(_) ->
+            [{"b", "blue", cold}] = rm_cache:to_list(t5) end).
+
+
+hot_key(Cache, Key) ->
+    {ok, V} = rm_cache:lookup(Cache, Key), % cold after first lookup
+    {ok, V} = rm_cache:lookup(Cache, Key). % officially hot
